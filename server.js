97d41e491e9d620ab119499a207d695fb49b6857
sanfranciscodeasis <mireyasantiago105@gmail.com>
2017-08-26 23:32:30 -0500

    como subir servicios estaticos con espress

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express'); //con require se buscara express dentro de nodemodules
 var app= express();
+//para ver se utilizara pug
+app.set('view engine', 'pug');
+
+//para que cualquier usuario pueda acceder a el 
+app.use(express.static('public'));
 // definiremos la ruta
 app.get('/', function(req, res){
-  res.send('hola');
+  //res.send('hola');
+  //.render quiere decir que llamara el punto de vista
+  res.render('index')
 })
 
 app.listen(3000, function(err){
